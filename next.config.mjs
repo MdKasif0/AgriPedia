63063f4029eb1428d483a8f38a49f685c3eb4d95
MdKasif0 <mdkasifuddin121@gmail.com>
2025-06-03 10:28:37 +0530

    fix: Update Next.js configuration and add Netlify config for deployment

---
+++ next.config.mjs
@@ -0,0 +1,85 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.clevelandclinic.org',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.rhs.org.uk',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/manifest.webmanifest',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/manifest+json',
+          },
+        ],
+      },
+      {
+        source: '/sw.js',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+          },
+          { 
+            key: 'Pragma', 
+            value: 'no-cache' 
+          },
+          { 
+            key: 'Expires', 
+            value: '0' 
+          },
+          {
+            key: 'Service-Worker-Allowed',
+            value: '/',
+          }
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig; 
